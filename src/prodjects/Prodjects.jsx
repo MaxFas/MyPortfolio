8ae4b474b9beea9e796583213f1343ac9fb27d4d
MaxFas <FastsovMax@yandex.ru>
2021-07-19 18:51:23 +0300

    add project

--- src/prodjects/Prodjects.jsx
+++ src/prodjects/Prodjects.jsx
@@ -4,12 +4,17 @@ import Project from "./project/Prodject";
 import Title from "../commons/components/Title";
 import tdlImg from "./../assets/imgs/tdl.jpg"
 import snImg from "./../assets/imgs/sn.jpg"
+import sbImg from "./../assets/imgs/lib.jpg"
 
 function Projects() {
 
     const tdl= {
         color: 'blue',
         backgroundImage: `url(${tdlImg})`,
+    };
+    const sb= {
+        color: 'blue',
+        backgroundImage: `url(${sbImg})`,
     };
 
 
@@ -27,6 +32,9 @@ function Projects() {
                              style={social} description={"Gradual implementation of a full-fledged social network (login, displaying users, sending messages, creating your own page)"}/>
                     <Project title={'To-Do List'} link={'https://maxfas.github.io/ToDoList/'} style={tdl}
                              description={"An application where you can plan your tasks list. Implemented remove, create and change tasks.Also done work on error handling"}/>
+                     <Project title={'To-Do List'} link={'https://maxfas.github.io/ToDoList/'} style={sb}
+                             description={"A Google Books API-related app that makes it easy to find any book"}/>
+
                 </div>
             </div>
         </div>
